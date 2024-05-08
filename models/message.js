76fe3110a0c25c33c5599ef53a3203f14e97f283
Luacrative <afif317@gmail.com>
2024-05-08 00:17:54 -0700

    Add message replies template

--- models/message.js
+++ models/message.js
@@ -4,7 +4,8 @@ const message = new mongoose.Schema({
     user: {type: mongoose.Schema.ObjectId, ref: "User", required: true},
     title: {type: String, required: true},
     content: {type: String, required: true},
-    date: {type: Date, required: true}
+    date: {type: Date, required: true},
+    replies: [{type: mongoose.Schema.ObjectId}]
 });
 
 module.exports = mongoose.model("Message", message); 
